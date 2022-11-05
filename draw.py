221662b249d669b3cab1e141972e41d6c1785720
Victor <viktor.119.1853@gmail.com>
2022-11-05 17:31:27 +0300

    pyqt5

--- draw.py
+++ draw.py
@@ -1,34 +1,30 @@
-import pygame
-import math
-from CONST import *
+import sys
 
-pygame.init()
-pygame.mixer.init()
-sc = pygame.display.set_mode((OX, OY), pygame.RESIZABLE)
-pygame.display.set_caption('engl')
-clock = pygame.time.Clock()
-text = ''
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_BACKSPACE:
-                text = text[:-1]
-            else:
-                text += event.unicode
-        if event.type == pygame.MOUSEMOTION:
-            pass
-    # key = pygame.key.get_pressed()
-    # if key[pygame.K_BACKSPACE]:
-    #     text = text[:-1]
 
-    font = pygame.font.Font(None, 50)
-    text_draw = font.render(text, True, (0, 100, 0))
-    sc.fill((255, 255, 255))
-    place = text_draw.get_rect(
-        center=(OX//2, OY*3//4))
-    sc.blit(text_draw, place)
-    clock.tick(FPS)
-    pygame.display.update()
+class Ui_Settings(object):
+    def setupUi(self, Settings):
+        Settings.setObjectName("Settings")
+        Settings.resize(300, 300)
+
+
+class Settings(QtWidgets.QWidget, Ui_Settings):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def keyPressEvent(self, event):
+        if event.nativeVirtualKey() == Qt.Key_F:
+            self.test()
+
+    def test(self):
+        print("Hello world")
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = Settings()
+    MainWindow.show()
+    sys.exit(app.exec_())
